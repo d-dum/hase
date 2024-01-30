c5cf669a3542db8f5514e350987722a8f2c50a94
d-dum <levdenisov666@gmail.com>
2024-01-30 23:35:52 +0100

    format file with dformat

--- source/hase/core/engine.d
+++ source/hase/core/engine.d
@@ -7,55 +7,54 @@ import std.conv : to;
 
 interface IEngine
 {
-    void engineLoop(void delegate() callback);
-    IWindow getWindow();
+	void engineLoop(void delegate() callback);
+	IWindow getWindow();
 }
 
 alias Hase = Engine!(HaseWindow, 4, 6);
 
-template GenGLVersion(int openglMajor, int openglMinor)
+template genGLVersion(int openglMajor, int openglMinor)
 {
-    const char[] GenGLVersion = "GLSupport.gl" ~ to!string(openglMajor) ~ to!string(openglMinor);
+	const char[] genGLVersion = "GLSupport.gl" ~ to!string(openglMajor) ~ to!string(openglMinor);
 }
 
 class Engine(W : IWindow, int openglMajor, int openglMinor) : IEngine
 {
-    static assert(openglMajor >= 3 && openglMajor <= 4, "OpenGL major version must be >= 3 and <= 4");
-    static assert(openglMinor <= 6 && openglMinor >= 0, "OpenGL minor version must be <= 6 and >= 0");
+	static assert(openglMajor >= 3 && openglMajor <= 4, "OpenGL major version must be >= 3 and <= 4");
+	static assert(openglMinor <= 6 && openglMinor >= 0, "OpenGL minor version must be <= 6 and >= 0");
 
 private:
-    W window;
-
+	W window;
 
 public:
 
-    this(int width, int height, string name)
-    {
-        window = new W(width, height, name);
+	this(int width, int height, string name)
+	{
+		window = new W(width, height, name);
 
-        GLSupport ret = loadOpenGL();
-        if(ret == GLSupport.noLibrary || ret == GLSupport.badLibrary || ret == GLSupport.noContext)
-        {
-            throw new Exception("Failed to load opengl: " ~ ret.stringof);
-        }
-        assert(ret == mixin(GenGLVersion!(openglMajor, openglMinor)), "Failed to create context of correct version");
+		GLSupport ret = loadOpenGL();
+		if (ret == GLSupport.noLibrary || ret == GLSupport.badLibrary || ret == GLSupport.noContext)
+		{
+			throw new Exception("Failed to load opengl: " ~ ret.stringof);
+		}
+		assert(ret == mixin(genGLVersion!(openglMajor, openglMinor)), "Failed to create context of correct version");
 
-    }
+	}
 
-    void engineLoop(void delegate() callback)
-    {
-        while(!window.isCloseRequested())
-        {
-            window.pollEvents();
+	void engineLoop(void delegate() callback)
+	{
+		while (!window.isCloseRequested())
+		{
+			window.pollEvents();
 
-            callback();
+			callback();
 
-            window.update();
-        }
-    }
+			window.update();
+		}
+	}
 
-    W getWindow()
-    {
-        return window;
-    }
+	W getWindow()
+	{
+		return window;
+	}
 }
